0ac39f33f732f879c34deaf1122f8c54d1cc77db
Alexander Mann-Wahrenberg <alexander.mannwahrenberg@gmail.com>
2022-10-11 17:32:16 +0200

    Switch to theme material

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,22 @@
 project = "Hello Sphinx"
 copyright= "2022, Alexander Mann-Wahrenberg (basejumpa)"
 release = "0.1.0"
+
+html_theme = "sphinx_material"
+
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
+
+# Hide hyper link which leeds to the source of page displayed
+html_show_sourcelink = True
+
+html_theme_options = {
+    'nav_title'    : f"Write like an Egyptian",
+    'color_primary': 'teal',
+    'color_accent' : 'yellow',
+
+    'globaltoc_depth'        : 3,
+    'globaltoc_collapse'     : 'true',
+    'globaltoc_includehidden': 'true',
+}
--- docs/index.rst
+++ docs/index.rst
@@ -6,3 +6,9 @@ Hello Sphinx!
 .. figure:: _figures/2022-10-11-17-13-29.png
 
 .. figure:: _figures/2022-10-11-17-13-59.png
+
+.. toctree::
+    :maxdepth: 1
+
+    anotherfile
+    someotherfile
